206a6ae97c647911838ce31eeece441da8213cb6
suntiecheng <suntiecheng@baidu.com>
2016-05-23 17:55:33 +0800

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+// var query = document.querySelector;
 
 var Hello = React.createClass({
 	render: function() {
@@ -7,4 +8,50 @@ var Hello = React.createClass({
 	}
 });
 
+var List = React.createClass({
+    makeGroup: function(val) {
+        return <li key={val.key}>{val.text}</li>;
+    },
+    render: function() {
+        return (
+            <ul>
+                {this.props.items.map(this.makeGroup)}
+            </ul>
+        );
+    }
+});
+
+var TodoApp = React.createClass({
+    getInitialState: function() {
+        return {
+            text: '',
+            items: []
+        }
+    },
+    handleChange: function(e) {
+        this.setState({
+            text: e.target.value
+        });
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        this.setState({
+            items: this.state.items.concat([{key: +new Date, text: this.state.text}]),
+            text: ''
+        });
+    },
+    render: function() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange} value={this.state.text} />
+                    <button type="submit">Add {this.state.items.length}</button>
+                </form>
+                <List items={this.state.items} />
+            </div>
+        );
+    }
+});
+
 ReactDom.render(<Hello name="tiecheng" />, document.querySelector('#box'));
+ReactDom.render(<TodoApp />, document.querySelector('#todo-container'));
